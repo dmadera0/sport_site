ed4b91b76d5ce8955e07fd956e370c28bc94aa8e
daniel <dmadera0@gmail.com>
2025-05-13 09:51:07 -0700

    added logo image

--- js/script.js
+++ js/script.js
@@ -2,5 +2,14 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Sports webpage loaded');
-    // You can add more JavaScript functionality here
+    
+    // Highlight the active link
+    const currentPage = window.location.pathname.split('/').pop();
+    const navLinks = document.querySelectorAll('nav ul li a');
+    
+    navLinks.forEach(link => {
+        if (link.getAttribute('href') === currentPage) {
+            link.classList.add('active');
+        }
+    });
 });
