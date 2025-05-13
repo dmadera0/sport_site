83c7a1b5c2a741b83652d0be4e3b4e8a1959bf71
daniel <dmadera0@gmail.com>
2025-05-13 09:22:05 -0700

    added html files for links

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Dummy JavaScript file for sports webpage
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Sports webpage loaded');
+    // You can add more JavaScript functionality here
+});
